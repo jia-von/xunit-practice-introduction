ce794e688cb843430f0c916974d40a0e3b39e6eb
Jia <jiavon@ualberta.ca>
2020-09-08 13:55:28 -0600

    invalidvalues testing has passed

--- Program.cs
+++ Program.cs
@@ -26,5 +26,46 @@ namespace XUnitIntroPractice
                 return false;
             }
         }
+
+        public static bool IsYesOrNoException(string input)
+        {
+            if (input.ToUpper().Trim() == "YES" || input.ToUpper().Trim() == "Y" || input.ToUpper().Trim() == "NO" || input.ToUpper().Trim() == "N")
+            {
+                return true;
+            }
+            else
+            {
+                throw new FormatException();
+            }
+        }
+
+        public static void OutOfRange()
+        {
+            int[] values1 = { 3, 6, 9, 12, 15, 18, 21 };
+            int[] values2 = new int[6];
+            
+            
+            values2[values1.Length - 1] = values1[values1.Length - 1];
+        }
+
+        public static void NullTest()
+        {
+            int[] values = null;
+            for (int ctr = 0; ctr <= 9; ctr++)
+                values[ctr] = ctr * 2;
+
+            foreach (var value in values)
+                Console.WriteLine(value);
+        }
+
+        public static void ZeroTest()
+        {
+            int number1 = 3;
+            int number2 = 0;
+
+           int sum = number1 / number2;
+
+        }
+
     }
 }
