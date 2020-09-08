6d931d98b99446a028c998188beedd9e85cf8a9a
Jia <jiavon@ualberta.ca>
2020-09-08 11:22:17 -0600

    Completed IsYesOrNo method that accept string and return either true or false.

--- Program.cs
+++ Program.cs
@@ -2,11 +2,22 @@
 
 namespace XUnitIntroPractice
 {
-    class Program
+    public class Program //Update the Program class to be public, so the test framework can see it.
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            //Write a method called IsYesOrNo that will accept a string and return true if the string is “YES”, “Y”, “NO”, or “N”, in any casing, trimmed. Otherwise it will return false.
+            static bool IsYesOrNo(string input)
+            {
+                if (input.ToLower().Trim() == "yes" || input.ToLower().Trim() == "y" || input.ToLower().Trim() == "no" || input.ToLower().Trim() == "n")
+                {
+                    return true;
+                }
+                else
+                {
+                    return false;
+                }
+            }
         }
     }
 }
